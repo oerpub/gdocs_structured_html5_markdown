1dd109c0fe8bec26ba4c3f54b5c778091db5cc45
Marvin Reimer <therealmarv@gmail.com>
2017-02-18 19:22:24 +0800

    remove debug direcories

--- gdocs_debug_tools/gdocsdownload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Use GoogleCL to download a document from your Google Docs repository
-google docs get --title "$1" $1.htm
--- gdocs_debug_tools/gdocstidy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#HTML tidy a Google Docs document
-#Usage ./gdocstidy.sh doc.htm doc_tidied.htm
-tidy -config tidyconfig.cfg -o $2 $1 
