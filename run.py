138f2714ae120b9149a86490305ac8c4d5e7078a
Marvin Reimer <therealmarv@gmail.com>
2017-05-03 12:11:02 +0300

    fix utf 8 problems

--- run.py
+++ run.py
@@ -2,4 +2,10 @@
 """Flask web server"""
 
 from app import app
+
+# fix flask utf-8 problems http://stackoverflow.com/a/14919377/756056
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 app.run(debug=True)
